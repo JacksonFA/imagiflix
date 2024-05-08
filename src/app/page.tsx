84b6be6cf23858f1715cff59c211517e2f8cfc90
omaxfernandes <max.sfernandes@outlook.com>
2024-05-08 18:27:02 -0300

    refactor(responsivity): adding responsiveness to the login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,28 +14,28 @@ export default function Home() {
         sizes="100vw"
         className="-z-10 object-cover brightness-50"
       />
-      <main className="grid grid-cols-3 pt-10">
+      <main className="grid mobile:grid-cols-1 leptops:grid-cols-3  tablet:grid-cols-1 pt-10">
         <Image src={Logo} alt="Imagiflix Logo" className="ml-8" />
-        <section className="bg-zinc-900/90 max-w-[600px] min-h-[800px] p-10 rounded-xl">
-          <h3 className="text-zinc-50 font-black text-5xl">Entrar</h3>
-          <form className="flex flex-col gap-14 mt-12">
+        <section className="bg-zinc-900/90  leptops:p-10 mobile:p-6 rounded-xl leptops:w-[600px] leptops:h-[800px] mobile:w-[310px] mobile:h-[500px] mobile:mx-auto mobile:mt-20 leptops:mt-0">
+          <h3 className="text-zinc-50 font-black leptops:text-5xl mobile:text-2xl">Entrar</h3>
+          <form className="flex flex-col leptops:gap-14 mobile:gap-6 mt-12">
             <input
-              className="p-8 bg-zinc-500 rounded-xl placeholder:text-zinc-300"
+              className="leptops:p-8 mobile:p-3 bg-zinc-500 rounded-xl placeholder:text-zinc-300"
               type="email"
               placeholder="Email ou número"
             />
             <input
-              className="p-8 bg-zinc-500 rounded-xl placeholder:text-zinc-300"
+              className="leptops:p-8 mobile:p-3 bg-zinc-500 rounded-xl placeholder:text-zinc-300"
               type="password"
               placeholder="Sua senha"
             />
             <button
-              className="p-8 mt-4 bg-red-600 text-zinc-50 rounded-xl font-extrabold text-3xl"
+              className="leptops:p-8 mobile:p-3 8 mt-4 bg-red-600 text-zinc-50 rounded-xl font-extrabold leptops:text-3xl mobile:text-sm hover:bg-red-800"
               type="submit"
             >
               Entrar
             </button>
-            <span className="mt-8 font-extralight text-xl text-zinc-300 flex flex-col gap-4">
+            <span className="leptops:mt-8 mobile:mt-4 font-extralight leptops:text-xl mobile:text-sm text-zinc-300 flex flex-col gap-4">
               <p>
                 Novo na Imagiflix?{' '}
                 <a className="text-indigo-600 font-bold ml-6" href="#">
