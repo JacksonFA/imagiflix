39c8ad52bd1d3d4068c8003ca2fa4e1d9438eaa8
JacksonFA <jackson.arceno@seventh.com.br>
2024-06-26 21:56:59 -0300

    fix(next_api): undo comments

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,72 +4,74 @@ import { Category } from '@/components/category/Category'
 import { Movie } from '@/types/movie'
 import { randomInt } from 'node:crypto'
 
-// async function getPopularMovies(): Promise<Movie[]> {
-//   const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/movie/popular', {
-//     cache: 'no-cache',
-//   })
-//   return response.json()
-// }
+async function getPopularMovies(): Promise<Movie[]> {
+  const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/movie/popular', {
+    cache: 'no-cache',
+  })
+  return response.json()
+}
 
-// async function getPopularSeries(): Promise<Movie[]> {
-//   const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/tv/popular', {
-//     cache: 'no-cache',
-//   })
-//   return response.json()
-// }
+async function getPopularSeries(): Promise<Movie[]> {
+  const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/tv/popular', {
+    cache: 'no-cache',
+  })
+  return response.json()
+}
 
-// async function getTopRatedMovies(): Promise<Movie[]> {
-//   const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/movie/top_rated', {
-//     cache: 'no-cache',
-//   })
-//   return response.json()
-// }
+async function getTopRatedMovies(): Promise<Movie[]> {
+  const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/movie/top_rated', {
+    cache: 'no-cache',
+  })
+  return response.json()
+}
 
-// async function getTopRatedSeries(): Promise<Movie[]> {
-//   const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/tv/top_rated', {
-//     cache: 'no-cache',
-//   })
-//   return response.json()
-// }
+async function getTopRatedSeries(): Promise<Movie[]> {
+  const response = await fetch('${process.env.NEXT_PUBLIC_API_URL}/tmdb/tv/top_rated', {
+    cache: 'no-cache',
+  })
+  return response.json()
+}
 
-// async function getHeroContent(): Promise<Movie & { type: 'tv' | 'movie' }> {
-//   const types = ['movie', 'tv']
-//   const type = types[randomInt(2)]
-//   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tmdb/${type}/popular`, {
-//     cache: 'no-cache',
-//   })
-//   const results = await response.json()
-//   const item = { ...results[randomInt(results?.length)], type }
-//   return item
-// }
+async function getHeroContent(): Promise<Movie & { type: 'tv' | 'movie' }> {
+  const types = ['movie', 'tv']
+  const type = types[randomInt(2)]
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tmdb/${type}/popular`, {
+    cache: 'no-cache',
+  })
+  const results = await response.json()
+  const item = { ...results[randomInt(results?.length)], type }
+  return item
+}
 
 export default async function Main() {
-  return <></>
-  // const [popularMovies, popularSeries, topRatedMovies, topRatedSeries, heroContent] =
-  //   await Promise.all([
-  //     getPopularMovies(),
-  //     getPopularSeries(),
-  //     getTopRatedMovies(),
-  //     getTopRatedSeries(),
-  //     getHeroContent(),
-  //   ])
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+    throw new Error('NEXT_PUBLIC_API_URL is not defined')
+  }
+  const [popularMovies, popularSeries, topRatedMovies, topRatedSeries, heroContent] =
+    await Promise.all([
+      getPopularMovies(),
+      getPopularSeries(),
+      getTopRatedMovies(),
+      getTopRatedSeries(),
+      getHeroContent(),
+    ])
 
-  // return (
-  //   <main className="bg-zinc-950">
-  //     <Hero
-  //       type={heroContent.type}
-  //       id={heroContent.id}
-  //       title={heroContent.title!}
-  //       overview={heroContent.overview}
-  //       backdrop_path={heroContent.backdrop_path}
-  //     />
-  //     <Header />
-  //     <div className="mt-[82vh] flex flex-col gap-8 pb-8 pl-16">
-  //       <Category title="Filmes em alta" list={popularMovies} type="movie" />
-  //       <Category title="Séries em alta" list={popularSeries} type="tv" />
-  //       <Category title="Filmes mais bem avaliados" list={topRatedMovies} type="movie" />
-  //       <Category title="Séries mais bem avaliadas" list={topRatedSeries} type="tv" />
-  //     </div>
-  //   </main>
-  // )
+  return (
+    <main className="bg-zinc-950">
+      <Hero
+        type={heroContent.type}
+        id={heroContent.id}
+        title={heroContent.title!}
+        overview={heroContent.overview}
+        backdrop_path={heroContent.backdrop_path}
+      />
+      <Header />
+      <div className="mt-[82vh] flex flex-col gap-8 pb-8 pl-16">
+        <Category title="Filmes em alta" list={popularMovies} type="movie" />
+        <Category title="Séries em alta" list={popularSeries} type="tv" />
+        <Category title="Filmes mais bem avaliados" list={topRatedMovies} type="movie" />
+        <Category title="Séries mais bem avaliadas" list={topRatedSeries} type="tv" />
+      </div>
+    </main>
+  )
 }
